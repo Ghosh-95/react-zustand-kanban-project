7c4fa08aeefb140f940525a823f385f04d84593d
Sushovan <sushovanghosh960@gmail.com>
2024-04-23 14:20:10 +0530

    feat: implement a function add a task to the tasks array

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 
 const store = (set) => ({
-    tasks: [{ title: "TestTask", state: "Planned" }],
+    tasks: [{ title: "TestTask", state: "planned" }],
+    addTask: (state, title) => set(store => ({ tasks: [...store.tasks, { state, title }] }))
 });
 
 export const myStore = create(store);
