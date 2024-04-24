c16bc936b72beb059d312d61166633392b42598b
Sushovan <sushovanghosh960@gmail.com>
2024-04-24 09:32:57 +0530

    add 'devtools' method from zustand middleware to access Redux Devtool Extension
    add other parameters to set function inside store for lableling all the action

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 function generateId() {
     const chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%&+';
@@ -12,10 +13,10 @@ function generateId() {
 const store = (set) => ({
     tasks: [{ title: "TestTask", state: "planned", id: generateId() }],
     dragTask: null,
-    addTask: (state, title, id = generateId()) => set(store => ({ tasks: [...store.tasks, { state, title, id }] })),
-    deleteTask: (id) => set(store => ({ tasks: store.tasks.filter(task => task.id !== id) })),
-    setDragTask: (id) => set({ dragTask: id }),
-    moveTask: (id, state) => set(store => ({ tasks: store.tasks.map(task => task.id === id ? { id, state, title: task.title } : task) }))
+    addTask: (state, title, id = generateId()) => set(store => ({ tasks: [...store.tasks, { state, title, id }] }), false, "tasks/addTask"),
+    deleteTask: (id) => set(store => ({ tasks: store.tasks.filter(task => task.id !== id) }), false, "tasks/deleteTask"),
+    setDragTask: (id) => set({ dragTask: id }, false, "tasks/setDragTask"),
+    moveTask: (id, state) => set(store => ({ tasks: store.tasks.map(task => task.id === id ? { id, state, title: task.title } : task) }), false, "tasks/moveTask")
 });
 
-export const myStore = create(store);
+export const myStore = create(devtools(store));
